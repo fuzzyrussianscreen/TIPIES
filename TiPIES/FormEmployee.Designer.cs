b868abb69bf7901d33024f4b7a5c90f414587763
OkCur64 OkCur64 <ok.curilo2010@yandex.ru>
2020-01-13 18:07:26 +0400

    1232

--- TiPIES/FormEmployee.Designer.cs
+++ TiPIES/FormEmployee.Designer.cs
@@ -125,7 +125,7 @@
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(12, 30);
+            this.dataGridView1.Location = new System.Drawing.Point(21, 30);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 51;
